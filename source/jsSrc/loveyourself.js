97339ec93fbff622737fef373f5c82cdcf1f301a
it63070045 <63070045@kmitl.ac.th>
2021-11-16 21:35:08 +0700

    undate all

--- source/jsSrc/loveyourself.js
+++ source/jsSrc/loveyourself.js
@@ -7,38 +7,43 @@ fetch('/source/contentSrc/english.json')
 )
 function play(){
     document.getElementById('play').style.display = "none";
+    document.getElementById('photovidco').style.display = "none";
     document.getElementById('vid').style.display = "block";
     document.getElementById('vid').src = "https://www.youtube.com/embed/fJ9rUzIMcZQ?autoplay=1"
 }
 function vid1() {
     document.getElementById('play').style.display = "none";
     document.getElementById('vid').style.display = "block";
-
+    document.getElementById('photovidco').style.display = "none";
     document.getElementById('vid').src = "https://www.youtube.com/embed/AJEoJYgktb4?autoplay=1"
     }
 function vid2() {
     document.getElementById('play').style.display = "none";
     document.getElementById('vid').style.display = "block";
-
+    document.getElementById('photovidco').style.display = "none";
     document.getElementById('vid').src = "https://www.youtube.com/embed/9hhMUT2U2L4?autoplay=1"
     }
 function vid3() {
     document.getElementById('play').style.display = "none";
+    document.getElementById('photovidco').style.display = "none";
     document.getElementById('vid').style.display = "block";
     document.getElementById('vid').src = "https://www.youtube.com/embed/aTJncWndUB8?autoplay=1"    
     }
 function vid4() {
     document.getElementById('play').style.display = "none";
+    document.getElementById('photovidco').style.display = "none";
     document.getElementById('vid').style.display = "block";
     document.getElementById('vid').src = "https://www.youtube.com/embed/qjmv57QMQzg?autoplay=1" 
     }
 function vid5() {
     document.getElementById('play').style.display = "none";
+    document.getElementById('photovidco').style.display = "none";
     document.getElementById('vid').style.display = "block";
     document.getElementById('vid').src = "https://www.youtube.com/embed/G-uqRYqzmXw?autoplay=1"     
     }
 function vid6() {
     document.getElementById('play').style.display = "none";
+    document.getElementById('photovidco').style.display = "none";
     document.getElementById('vid').style.display = "block";
     document.getElementById('vid').src = "https://www.youtube.com/embed/fJ9rUzIMcZQ?autoplay=1"     
     }
