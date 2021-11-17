ca55a2f205301191e5a9e20211ea4fddea677200
Whitewolf <nonggram45@gmail.com>
2021-11-17 16:53:55 +0700

    Update

---
+++ source/jsSrc/loveyourself.js
@@ -0,0 +1,182 @@
+function load_content(data) {
+    let data_content = data["lysh_album"];
+    document.getElementById("data").innerHTML = data_content["paragraph"];
+}
+
+function play() {
+    document.getElementById('vidbut6').style.opacity = "1";
+    document.getElementById('play').style.display = "none";
+    document.getElementById('photovidco').style.display = "none";
+    document.getElementById('vid').style.display = "block";
+    document.getElementById('vid').src = "https://www.youtube.com/embed/kTlv5_Bs8aw?autoplay=1"
+}
+
+function vid1() {
+    document.getElementById('play').style.display = "none";
+    document.getElementById('vid').style.display = "block";
+    document.getElementById('photovidco').style.display = "none";
+    document.getElementById('vidbut1').style.opacity = "1";
+    document.getElementById('vidbut2').style.opacity = "50%";
+    document.getElementById('vidbut3').style.opacity = "50%";
+    document.getElementById('vidbut4').style.opacity = "50%";
+    document.getElementById('vidbut5').style.opacity = "50%";
+    document.getElementById('vidbut6').style.opacity = "50%";
+    document.getElementById('vid').src = "https://www.youtube.com/embed/BEMaH9Sm3lQ?autoplay=1"
+}
+
+function vid2() {
+    document.getElementById('play').style.display = "none";
+    document.getElementById('vid').style.display = "block";
+    document.getElementById('photovidco').style.display = "none";
+    document.getElementById('vidbut1').style.opacity = "50%";
+    document.getElementById('vidbut2').style.opacity = "1";
+    document.getElementById('vidbut3').style.opacity = "50%";
+    document.getElementById('vidbut4').style.opacity = "50%";
+    document.getElementById('vidbut5').style.opacity = "50%";
+    document.getElementById('vidbut6').style.opacity = "50%";
+    document.getElementById('vid').src = "https://www.youtube.com/embed/FSOxXsYVJMQ?autoplay=1"
+}
+
+function vid3() {
+    document.getElementById('play').style.display = "none";
+    document.getElementById('photovidco').style.display = "none";
+    document.getElementById('vid').style.display = "block";
+    document.getElementById('vidbut1').style.opacity = "50%";
+    document.getElementById('vidbut2').style.opacity = "50%";
+    document.getElementById('vidbut3').style.opacity = "1";
+    document.getElementById('vidbut4').style.opacity = "50%";
+    document.getElementById('vidbut5').style.opacity = "50%";
+    document.getElementById('vidbut6').style.opacity = "50%";
+    document.getElementById('vid').src = "https://www.youtube.com/embed/g_wuoInNyAI?autoplay=1"
+}
+
+function vid4() {
+    document.getElementById('play').style.display = "none";
+    document.getElementById('photovidco').style.display = "none";
+    document.getElementById('vid').style.display = "block";
+    document.getElementById('vidbut1').style.opacity = "50%";
+    document.getElementById('vidbut2').style.opacity = "50%";
+    document.getElementById('vidbut3').style.opacity = "50%";
+    document.getElementById('vidbut4').style.opacity = "1";
+    document.getElementById('vidbut5').style.opacity = "50%";
+    document.getElementById('vidbut6').style.opacity = "50%";
+    document.getElementById('vid').src = "https://www.youtube.com/embed/MBdVXkSdhwU?autoplay=1"
+}
+
+function vid5() {
+    document.getElementById('play').style.display = "none";
+    document.getElementById('photovidco').style.display = "none";
+    document.getElementById('vid').style.display = "block";
+    document.getElementById('vidbut1').style.opacity = "50%";
+    document.getElementById('vidbut2').style.opacity = "50%";
+    document.getElementById('vidbut3').style.opacity = "50%";
+    document.getElementById('vidbut4').style.opacity = "50%";
+    document.getElementById('vidbut5').style.opacity = "1";
+    document.getElementById('vidbut6').style.opacity = "50%";
+    document.getElementById('vid').src = "https://www.youtube.com/embed/ixxR3ZoqnF0?autoplay=1"
+}
+
+function vid6() {
+    document.getElementById('play').style.display = "none";
+    document.getElementById('photovidco').style.display = "none";
+    document.getElementById('vid').style.display = "block";
+    document.getElementById('vidbut1').style.opacity = "50%";
+    document.getElementById('vidbut2').style.opacity = "50%";
+    document.getElementById('vidbut3').style.opacity = "50%";
+    document.getElementById('vidbut4').style.opacity = "50%";
+    document.getElementById('vidbut5').style.opacity = "50%";
+    document.getElementById('vidbut6').style.opacity = "1";
+    document.getElementById('vid').src = "https://www.youtube.com/embed/kTlv5_Bs8aw?autoplay=1"
+}
+
+function phoset1() {
+    document.getElementById('phobut1').style.opacity = "1";
+    document.getElementById('phobut2').style.opacity = "50%";
+    document.getElementById('phobut3').style.opacity = "50%";
+    document.getElementById('phobut4').style.opacity = "50%";
+    document.getElementById('photo1').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset1/photo01-01.jpg"
+    document.getElementById('photo2').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset1/photo01-02.jpg"
+    document.getElementById('photo3').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset1/photo01-03.jpg"
+    document.getElementById('photo4').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset1/photo01-04.jpg"
+    document.getElementById('photo5').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset1/photo01-05.jpg"
+    document.getElementById('photo6').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset1/photo01-06.jpg"
+    document.getElementById('photo7').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset1/photo01-07.jpg"
+    document.getElementById('photo8').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset1/photo01-08.jpg"
+}
+
+function phoset2() {
+    document.getElementById('phobut1').style.opacity = "50%";
+    document.getElementById('phobut2').style.opacity = "1";
+    document.getElementById('phobut3').style.opacity = "50%";
+    document.getElementById('phobut4').style.opacity = "50%";
+    document.getElementById('photo1').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset2/photo02-01.jpg"
+    document.getElementById('photo2').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset2/photo02-02.jpg"
+    document.getElementById('photo3').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset2/photo02-03.jpg"
+    document.getElementById('photo4').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset2/photo02-04.jpg"
+    document.getElementById('photo5').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset2/photo02-05.jpg"
+    document.getElementById('photo6').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset2/photo02-06.jpg"
+    document.getElementById('photo7').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset2/photo02-07.jpg"
+    document.getElementById('photo8').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset2/photo02-08.jpg"
+}
+
+function phoset3() {
+    document.getElementById('phobut1').style.opacity = "50%";
+    document.getElementById('phobut2').style.opacity = "50%";
+    document.getElementById('phobut3').style.opacity = "1";
+    document.getElementById('phobut4').style.opacity = "50%";
+    document.getElementById('photo1').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset3/photo03-01.jpg"
+    document.getElementById('photo2').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset3/photo03-02.jpg"
+    document.getElementById('photo3').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset3/photo03-03.jpg"
+    document.getElementById('photo4').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset3/photo03-04.jpg"
+    document.getElementById('photo5').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset3/photo03-05.jpg"
+    document.getElementById('photo6').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset3/photo03-06.jpg"
+    document.getElementById('photo7').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset3/photo03-07.jpg"
+    document.getElementById('photo8').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset3/photo03-08.jpg"
+}
+
+function phoset4() {
+    document.getElementById('phobut1').style.opacity = "50%";
+    document.getElementById('phobut2').style.opacity = "50%";
+    document.getElementById('phobut3').style.opacity = "50%";
+    document.getElementById('phobut4').style.opacity = "1";
+    document.getElementById('photo1').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset4/photo04-01.jpg"
+    document.getElementById('photo2').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset4/photo04-02.jpg"
+    document.getElementById('photo3').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset4/photo04-03.jpg"
+    document.getElementById('photo4').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset4/photo04-04.jpg"
+    document.getElementById('photo5').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset4/photo04-05.jpg"
+    document.getElementById('photo6').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset4/photo04-06.jpg"
+    document.getElementById('photo7').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset4/photo04-07.jpg"
+    document.getElementById('photo8').src = "../../imgSrc/BTS/DISCOGRAPHY/LOVEYOURSELFHER/photoset4/photo04-08.jpg"
+}
+
+function mouseOver(x) {
+    x.style.color = "black";
+}
+
+function mouseOut(x) {
+    x.style.color = "#576686";
+}
+
+function mouseOver2(y) {
+    y.style.color = "black";
+}
+
+function mouseOut2(y) {
+    y.style.color = "#A6607F";
+}
+
+function mouseOver4() {
+    document.getElementById('left').style.color = "pink";
+}
+
+function mouseOut4() {
+    document.getElementById('left').style.color = "gray";
+}
+
+function mouseOver5() {
+    document.getElementById('right').style.color = "pink";
+}
+
+function mouseOut5() {
+    document.getElementById('right').style.color = "gray";
+}
